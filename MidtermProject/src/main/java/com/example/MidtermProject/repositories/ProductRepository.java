3d23ad089f99f7e10a39fd487ad44e047325e236
Zarina Kerimalieva <kerimalieva.zarina.2003@gmail.com>
2024-03-20 14:59:10 +0600

    initial commit

---
+++ MidtermProject/src/main/java/com/example/MidtermProject/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.MidtermProject.repositories;
+
+import com.example.MidtermProject.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitle(String title);
+}
