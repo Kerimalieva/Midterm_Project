39ade1f93f43024a6b3ce70fc09eee461543f012
Zarina Kerimalieva <kerimalieva.zarina.2003@gmail.com>
2024-03-23 01:23:44 +0600

    initial commit

--- MidtermProject/src/main/java/com/example/MidtermProject/controllers/UserController.java
+++ MidtermProject/src/main/java/com/example/MidtermProject/controllers/UserController.java
@@ -3,11 +3,13 @@ package com.example.MidtermProject.controllers;
 import com.example.MidtermProject.models.User;
 import com.example.MidtermProject.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.security.Principal;
 
@@ -35,6 +37,7 @@ public class UserController {
         model.addAttribute("user", userService.getUserByPrincipal(principal));
         return "registration";
     }
+
 
 
     @PostMapping("/registration")
